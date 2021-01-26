658ebbf154100be0292763cc37ecc92d2abafb21
Frank Casanova <frankcasanova.info@gmail.com>
2021-01-26 11:24:54 +0100

    valores javascript

---
+++ valores.js
@@ -0,0 +1,17 @@
+// valor numérico
+42
+
+// valor de texto
+'me llamo frank'
+
+// valores booleanos
+true
+false
+
+// valores vacios
+null
+undefined
+
+// valores tipos objetos
+[1, 2, 3]
+{ nombre: 'Frank' }
