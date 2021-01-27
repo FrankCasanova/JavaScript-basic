93a752e2158a301342830ab831f380c12f0bcd09
Frank Casanova <frankcasanova.info@gmail.com>
2021-01-27 12:51:33 +0100

    entendiendo las variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+// como se declaran las variables
+var nombre = 'Frank'
+// declarar
+var edad
+// inicializar
+edad = 30
+
+var elementos = ['Computadora', 'celular']
+
+var persona = {
+    nombre: 'Diego',
+    edad: 30
+}
+persona
+
+
+
+
+
+
+
+
+
+
